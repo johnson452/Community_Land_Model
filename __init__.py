98ef2b05c904b2f3f4849b3afb6a551b11cade8f
Grant Johnson <grj@princeton.edu>
2022-11-30 00:37:35 -0500

    Fixed model name, and __init__ linking

--- __init__.py
+++ __init__.py
@@ -9,5 +9,7 @@ __all__ = ["SRC"]
 import Community_Land_Model.SRC as SRC
 from SRC import Initialization
 from SRC import model_albedo
-from SRC import model_cloud
+from SRC import model_evaporation
+from SRC import model_absorbed_emitted_radiation
+from SRC import model_sensible_heat_flux
 from SRC import run_routine
