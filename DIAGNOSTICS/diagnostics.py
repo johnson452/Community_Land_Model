17657d0cf34d622647862a04eb8ca9bcb7cafbff
Grant Johnson <grj@princeton.edu>
2022-11-30 01:10:06 -0500

    Fixed: Diagnostics file reading

--- DIAGNOSTICS/diagnostics.py
+++ DIAGNOSTICS/diagnostics.py
@@ -6,15 +6,26 @@ Routines for analysing output data
 
 """
 
-# Ability to read in / write data
-App.load()
-State.load()
-Grid.load()
+import os
+import sys
 
-# Analyse and plot data
+script_dir = os.path.dirname(__file__)
+mymodule_dir = os.path.join(script_dir, "..", "SRC")
+sys.path.append(mymodule_dir)
+import initialization
+
+def diagnostics():
+    # Ability to read in / write data
+    App.load()
+    State.load()
+    Grid.load()
+
+    print("Read in the data properly")
+
+    # Analyse and plot data
 
 
-# Compute quanitities on data
+    # Compute quanitities on data
 
 
-# Plot commonly wanted things (T(t), etc.)
+    # Plot commonly wanted things (T(t), etc.)
