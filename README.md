c2cec3fb86ddb31e5fb4436e6d62e2a893799072
Grant Johnson <grj@princeton.edu>
2022-11-30 19:13:41 -0500

    Additional clarification in README.md, test_albedo

--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 Contributors: Kevin, Henry, Grant
 Date: 11/09/2022
 
-# Run a sample script
+# Run a sample script and diagnostics
 Download the code:
 >> git clone https://github.com/johnson452/Community_Land_Model
 
 Then run the following code:
 >> python3 EXAMPLES/input_script.py > OUTPUT/a.out
 
+Running diagnostics
+>> python3 DIAGNOSTICS/diagnostics.py
+
+this will save the plot outputs in the folder OUTPUT/PLOTS
+
 Check the code successfully ran with
 >> vim OUTPUT/a.out
 
-*** Before a pull request, check your branch with ***
+
+# Verify a commit before pushing
+Manual checks before committing
 >> nox -s tests
+
+and
 >> pre-commit run -a
--- TESTS/test_albedo.py
+++ TESTS/test_albedo.py
@@ -3,9 +3,28 @@
 Test for verifying the albedo module
 
 #Requires linking to the defintions:
-
+SRC/model_albedo.py
 """
 
+import os
+import sys
+import matplotlib.pyplot as plt
+
+script_dir = os.path.dirname(__file__)
+mymodule_dir = os.path.join(script_dir, "..", "SRC")
+sys.path.append(mymodule_dir)
+import model_albedo
+mymodule_dir = os.path.join(script_dir, "..", "EXAMPLES")
+sys.path.append(mymodule_dir)
+import input_script
+import initialization
 
 def test_albedo():
-    print("Albedo test ran, but currently has no contents\n")
+
+        # Build empty structures, App, State and Grid with blank data:
+        parameters = input_script.clm_parameters()
+        App, State, Grid = initialization.initialize_data(parameters)
+
+        # Maybe have a test_parameters script for an easy case
+
+        # Assert values for verified outputs (say for mu)
