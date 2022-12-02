93e688ae5a7c2e895aebd36be196bf1d92bf0ad2
Grant Johnson <grj@princeton.edu>
2022-12-01 21:23:48 -0500

    Fix Precommit

--- OUTPUT/README.md
+++ OUTPUT/README.md
@@ -1,2 +1,1 @@
 Output of the last simulation is stored in this folder
-
--- SRC/TABLES/table_3_1_plant_optics.py
+++ SRC/TABLES/table_3_1_plant_optics.py
@@ -51,7 +51,7 @@ def plant_props(plant_functional_type):
         Chi_L = 0.25
         alpha = [0.1, 0.45, 0.16, 0.39]
         tau = [0.05, 0.25, 0.001, 0.001]
-    else: #Grasslands and crops
+    else:  # Grasslands and crops
         Chi_L = -0.4
         alpha = [0.11, 0.35, 0.31, 0.53]
         tau = [0.05, 0.34, 0.120, 0.250]
