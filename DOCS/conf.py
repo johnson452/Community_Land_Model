6816c7d1e560540f74a768cd2d4f96380d349d94
Grant Johnson <grj@princeton.edu>
2022-12-10 16:12:44 -0500

    Precommit touchup

--- DOCS/conf.py
+++ DOCS/conf.py
@@ -6,28 +6,28 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Community Land Model'
-copyright = '2022, Grant Johnson, Kevin Yi, Henry Stone'
-author = 'Grant Johnson, Kevin Yi, Henry Stone'
-release = '1.0.0'
+project = "Community Land Model"
+copyright = "2022, Grant Johnson, Kevin Yi, Henry Stone"
+author = "Grant Johnson, Kevin Yi, Henry Stone"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
