28670a624f1f1e0fc8d6a2306cee4db6c4747073
Keivn <cy6164@princeton.edu>
2022-12-13 20:54:24 -0500

    small changes in existing files

--- SRC/model_sensible_heat_flux.py
+++ SRC/model_sensible_heat_flux.py
@@ -10,16 +10,12 @@ Outputs: None
 import os
 import sys
 import numpy as np
-import table_2_7_physical_constants
+import TABLES.table_2_7_physical_constants as table_2_7
 
 script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, ".", "TABLES")
 sys.path.append(mymodule_dir)
 
+
 def run_sensible_heat_flux_model(State, Grid, App, i):
     print("Running sensible_heat_flux Model\n")
-
-# Disregard the effects of surface water coververed area
-def sensible_heat_flux(State,Grid, App, i):
-    Cp = table_2_7_physical_constants.physical_constants("specific heat capacity of water")
-    
--- weather_data/Modeling_weather_data.m
+++ weather_data/Modeling_weather_data.m
@@ -3,9 +3,9 @@
 % misfit = 0.1454
 
 a = readcell("Weather_data_of_NJ.xlsx");
-data = zeros([365,3]);
+data = zeros([365,4]);
 
-%data matrix contains [day of the year, avg. temp., avg. humidity]
+%data matrix contains [day of the year, avg. windspeed, avg. temp.]
 for i = 1 : 365
     data(i,1) = i;
 end
@@ -13,144 +13,168 @@ end
 for i = 2:378
     if a{i,1} == 'Jan'
         for k = 1:31
-            data(k,3) = a{k+i,9};
+            data(k,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Feb'
         for k = 1:28
-            data(k+31,3) = a{k+i,9};
+            data(k+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Mar'
         for k = 1:31
-            data(k+28+31,3) = a{k+i,9};
+            data(k+28+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Apr'
         for k = 1:30
-            data(k+28+31+31,3) = a{k+i,9};
+            data(k+28+31+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'May'
         for k = 1:31
-            data(k+28+31+31+30,3) = a{k+i,9};
+            data(k+28+31+31+30,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Jun'
         for k = 1:30
-            data(k+31+28+31+31+30,3) = a{k+i,9};
+            data(k+31+28+31+31+30,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Jul'
         for k = 1:31
-            data(k+31+28+31+31+30+30,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Aug'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Sep'
         for k = 1:30
-            data(k+31+28+31+31+30+30+31+31,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30+31+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Oct'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31+31+30,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30+31+31+30,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Nov'
         for k = 1:30
-            data(k+31+28+31+31+30+30+31+31+30+31,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30+31+31+30+31,3) = a{k+i,3};
         end
     end
     if a{i,1} == 'Dec'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31+31+30+31+30,3) = a{k+i,9};
+            data(k+31+28+31+31+30+30+31+31+30+31+30,3) = a{k+i,3};
         end
     end
 end
 
 
 
-%input humidity
+%input windspeed
 for i = 2:378
     if a{i,1} == 'Jan'
         for k = 1:31
-            data(k,2) = a{k+i,3};
+            data(k,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Feb'
         for k = 1:28
-            data(k+31,2) = a{k+i,3};
+            data(k+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Mar'
         for k = 1:31
-            data(k+28+31,2) = a{k+i,3};
+            data(k+28+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Apr'
         for k = 1:30
-            data(k+28+31+31,2) = a{k+i,3};
+            data(k+28+31+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'May'
         for k = 1:31
-            data(k+28+31+31+30,2) = a{k+i,3};
+            data(k+28+31+31+30,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Jun'
         for k = 1:30
-            data(k+31+28+31+31+30,2) = a{k+i,3};
+            data(k+31+28+31+31+30,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Jul'
         for k = 1:31
-            data(k+31+28+31+31+30+30,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Aug'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Sep'
         for k = 1:30
-            data(k+31+28+31+31+30+30+31+31,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30+31+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Oct'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31+31+30,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30+31+31+30,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Nov'
         for k = 1:30
-            data(k+31+28+31+31+30+30+31+31+30+31,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30+31+31+30+31,2) = a{k+i,12};
         end
     end
     if a{i,1} == 'Dec'
         for k = 1:31
-            data(k+31+28+31+31+30+30+31+31+30+31+30,2) = a{k+i,3};
+            data(k+31+28+31+31+30+30+31+31+30+31+30,2) = a{k+i,12};
         end
     end
 end
 
+
 %Modeling using inverse method
 %Assume the period of temperature changing is 365days via observing
+%periodogram(data(:,2)-mean(data(:,2)),hann(length(data(:,2))))
 
+figure(1)
 T = 365;
 t = linspace(1,365,365);
 Ghat = [ones(365,1), cos(2*pi*t/T)', sin(2*pi*t/T)'];
-mhat = pinv(Ghat)*data(:,2);
+mhat = pinv(Ghat)*data(:,3);
 Temphat = Ghat * mhat;
+plot(data(:,1),Temphat)
+hold on
+plot(data(:,1),data(:,3))
+%calculating variance reduction
 
-%calculating misfit
+phiT = var(Temphat-data(:,3))/var(data(:,3));
 
-phiT = var(Temphat-data(:,2))/var(data(:,2));
+mean(data(:,2))
+std(data(:,2))
+figure(2)
+plot(())
+% figure(2)
+% periodogram(data(:,2) - mean(data(:,2)))
+% %peak at 0.003906
+%
+% figure(3)
+% T = 90;
+% t = linspace(1,365,365);
+% Ghat_w = [ones(365,1), cos(2*pi*t/T)', sin(2*pi*t/T)'];
+% mhat_w = pinv(Ghat_w)*data(:,2);
+% Windhat = Ghat_w * mhat_w;
+% plot(data(:,1),Windhat)
+% hold on
+% plot(data(:,1),data(:,2))
+
 
 %%visually examine temperature estimation by running the codes below
 % subplot(2,1,1)
@@ -159,9 +183,9 @@ phiT = var(Temphat-data(:,2))/var(data(:,2));
 % subplot(2,1,2)
 % plot(data(:,1),Temphat)
 
-% plot (data(:,1),data(:,3))
-GhatH = [ones(365,1), cos(2*pi*t/10)', sin(2*pi*t/10)'];
-mhatH = pinv(GhatH)*data(:,3);
-Hhat = GhatH * mhatH;
-phiH = var(Hhat-data(:,3))/var(data(:,3));
-plot(data(:,1),Hhat)
+% % plot (data(:,1),data(:,3))
+% GhatH = [ones(365,1), cos(2*pi*t/10)', sin(2*pi*t/10)'];
+% mhatH = pinv(GhatH)*data(:,3);
+% Hhat = GhatH * mhatH;
+% phiH = var(Hhat-data(:,3))/var(data(:,3));
+% plot(data(:,1),Hhat)
