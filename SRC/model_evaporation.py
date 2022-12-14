evaporation module bug fix, begin pytest, add heat gradient function in latent heat module

SRC/model_evaporation.py
@@ -23,6 +23,23 @@ import table_5_1_pft_aerodynamic as paero
 def run_evaporation_model(State, Grid, App, i):
     print("Running evaporation Model\n")
 
+    # Iterate 100 times
+    for i in range(100):
+        MO_length(State,Grid,App,i)
+        friction_velocity(State,Grid,App,i,psi)
+        temperature_ratio(State,Grid,App,i,psi)
+        humidity_ratio(State,Grid,App,i,psi)
+        air_res(State,Grid,App,i)
+        U_av(State,Grid,App,i)
+        q_sat(State,Grid,App,i)
+        r_b(State,Grid,App,i)
+        ra(State,Grid,App,i)
+        canopy_specific_humidity(State,Grid,App,i)
+        water_vapor_flux(State,Grid,App,i)
+        latent_heat_water_vapor_flux(State,Grid,App,i)
+        vegetation_water_vapor_flux(State,Grid,App,i)
+        
+        
 
 def water_vapor_flux(State, Grid, App, i):
     E = -1.2 * State.evaporation.humidity_ratio[i] / State.evaporation.ra[i]
@@ -31,7 +48,7 @@ def water_vapor_flux(State, Grid, App, i):
 
 
 def latent_heat_water_vapor_flux(State, Grid, App, i):
-    lmd = constants.physical_constants().constants("latent heat of vaporization")
+    lmd = constants.physical_constants().constants["latent heat of vaporization"]
     L_E = lmd * State.evaporation.E[i]
     State.evaporation.L_E[i] = L_E
     return L_E
@@ -51,7 +68,7 @@ def vegetation_water_vapor_flux(State, Grid, App, i):
 
 # Equation 5.46, 5.48 with modification using AMS infomation
 def MO_length(State, Grid, App, i):
-    z = af.atm_inputs().constants("thickness")
+    z = af.atm_inputs().constants["thickness"]
     Tv = 1.38 * State.evaporation.temperature[i]
     theta_v = 1.38 * State.evaporation.potential_temperature[i]
     R_iB = ((9.8 / Tv) * theta_v * z) / State.evaporation.windspeed[i] ** 2
@@ -75,9 +92,9 @@ def friction_velocity(State, Grid, App, i, psi):
     """
     L = State.evaporation.L[i]
     Ksi = State.evaporation.Ksi[i]
-    z = af.atm_inputs().constants("thickness")
-    z0 = af.atm_inputs().constants("roughness length")
-    k = constants.physical_constants().constants("von karman constant")
+    z = af.atm_inputs().constants["thickness"]
+    z0 = af.atm_inputs().constants["roughness length"]
+    k = constants.physical_constants().constants["von karman constant"]
     if Ksi < -1.574:
         Va = (
             State.evaporation.U_av[i]
@@ -104,38 +121,27 @@ def friction_velocity(State, Grid, App, i, psi):
 
 def q_sat(State, Grid, App, i):
     T = State.evaporation.temperature[i]
-    P = constants.physical_constants().constants("atmosphere pressure")
+    P = constants.physical_constants().constants["atmosphere pressure"]
     e_sat = 100 * (6.11 + 0.444 * T + 0.0143 * (T**2) + 0.000264 * (T**2))
     q_sat = (0.622 * e_sat) / (P - 0.378 * e_sat)
     State.evaporation.q_sat[i] = q_sat
     return q_sat
 
 
-def heat_flux_grad(State, Grid, App, i):
-    k = constants.physical_constants().constants("von karman constant")
-    Ksi = State.evaporation.Ksi[i]
-
-    if Ksi < -1.574:
-        phi = 0.9 * k ** (4 / 3) * ((-Ksi) ** (-1 / 3))
-    elif Ksi >= -1.574 and Ksi < 0:
-        phi = (1 - 16 * Ksi) ** (-0.5)
-    elif Ksi >= 0 and Ksi < 1:
-        phi = 1 + 5 * Ksi
-    else:
-        phi = 5 + Ksi
-    return phi
 
 
-def humidity_ratio(State, Grid, App, i, L, Ksi, psi):
+def humidity_ratio(State, Grid, App, i, psi):
     """
     L = MO_length(State,Grid,App,i)[1]
     Ksi = MO_length(State,Grid,App,i)[2]
     psi = psi(ksi)
 
     """
-    z0 = af.atm_inputs().constants("roughness length")
-    z = af.atm_inputs().constants("thickness")
-    k = constants.physical_constants().constants("von karman constant")
+    Ksi = State.evaporation.Ksi[i]
+    L = State.evaporation.L[i]
+    z0 = af.atm_inputs().constants["roughness length"]
+    z = af.atm_inputs().constants["thickness"]
+    k = constants.physical_constants().constants["von karman constant"]
     if Ksi < -1.574:
         del_q = (
             State.evaporation.specific_humidity[i]
@@ -168,16 +174,18 @@ def humidity_ratio(State, Grid, App, i, L, Ksi, psi):
     return del_q
 
 
-def temperature_ratio(State, Grid, App, i, L, Ksi, psi):
+def temperature_ratio(State, Grid, App, i, psi):
     """
     L = MO_length(State,Grid,App,i)[1]
     Ksi = MO_length(State,Grid,App,i)[2]
     psi = psi(ksi)
 
     """
-    z0 = af.atm_inputs().constants("roughness length")
-    z = af.atm_inputs().constants("thickness")
-    k = constants.physical_constants().constants("von karman constant")
+    Ksi = State.evaporation.Ksi[i]
+    L = State.evaporation.L[i]
+    z0 = af.atm_inputs().constants["roughness length"]
+    z = af.atm_inputs().constants["thickness"]
+    k = constants.physical_constants().constants["von karman constant"]
     if Ksi < -1.574:
         del_t = (
             State.evaporation.potential_temperature[i]
@@ -210,7 +218,9 @@ def temperature_ratio(State, Grid, App, i, L, Ksi, psi):
     return del_t
 
 
-def U_av(State, Grid, App, i, Va, rm):
+def U_av(State, Grid, App, i):
+    Va = State.evaporation.windspeed[i]
+    rm = State.evaporation.rm[i]
     U_av = Va * np.sqrt(1 / (rm * Va))
     State.evaporation.U_av[i] = U_av
     return U_av
@@ -244,42 +254,90 @@ def canopy_specific_humidity(State, Grid, App, i):
     State.evaporation.q_s[i] = q_s
     return q_s
 
+def air_res(State, Grid, App, i):
+    k = constants.physical_constants().constants["von karman constant"]
+    Ksi = State.evaporation.Ksi[i]
+    L = State.evaporation.L[i]
+    z0 = af.atm_inputs().constants["roughness length"]
+    z = af.atm_inputs().constants["thickness"]
+    if Ksi < -1.574:
+        del_q = (
+            1
+            / k
+            * (
+                (np.log(-1.574 * L / z0) - psi(-1.574))
+                + 1.14 * ((-Ksi) ** (1 / 3) - (1.574) ** (1 / 3))
+                + psi(z0 / L)
+            )
+        )
+    elif Ksi >= -1.574 and Ksi < 0:
+        del_q = (
+            1
+            / k
+            * ((np.log(z / z0) - psi(Ksi)) + psi(z0 / L))
+        )
+    elif Ksi >= 0 and Ksi < 1:
+        del_q = (
+            1
+            / k
+            * (np.log(z / z0) + 5 * Ksi - 5 * z0 / L)
+        )
+    else:
+        del_q = (
+            1
+            / k
+            * (np.log(L / z0) + 5 + 5 * np.log(Ksi) + Ksi - 1 - 5 * z0 / L)
+        )
+
+    if Ksi < -1.574:
+        del_t = (
+            1
+            / k
+            * (
+                (np.log(-1.574 * L / z0) - psi(-1.574))
+                + 1.14 * ((-Ksi) ** (1 / 3) - (1.574) ** (1 / 3))
+                + psi(z0 / L)
+            )
+        )
+    elif Ksi >= -1.574 and Ksi < 0:
+        del_t = (
+            1
+            / k
+            * ((np.log(z / z0) - psi(Ksi)) + psi(z0 / L))
+        )
+    elif Ksi >= 0 and Ksi < 1:
+        del_t = (
+            1
+            / k
+            * (np.log(z / z0) + 5 * Ksi - 5 * z0 / L)
+        )
+    else:
+        del_t = (
+            1
+            / k
+            * (np.log(L / z0) + 5 + 5 * np.log(Ksi) + Ksi - 1 - 5 * z0 / L)
+        )
+    rw = del_q/State.evaporation.U_av[i]
+    rm = State.evaporation.windspeed[i]/((State.evaporation.U_av[i]) ** (2))
+    rh = del_t / State.evaporation.U_av[i]
+    State.evaporation.rm[i] = rm
+    State.evaporation.rw[i] = rw
+    State.evaporation.rh[i] = rh
+    return (rw, rm, rh)
 
 # helper function
 
 # eqs 5.36
 def psi(ksi):
+    # only when x < 0 is the function called
     x = (1 - 16 * ksi) ** (1 / 4)
     return (
-        2 * np.log(1 + x / 2)
-        + np.log((1 + x**2) / 2)
+        2 * np.log((1 + x) / 2)
+        + np.log((1 + (x**2)) / 2)
         - 2 * (1 / np.tan(x))
         + np.pi / 2
     )
 
 
-def air_res(del_q, Va, del_T):
-    k = constants.physical_constants().constants("von karman constant")
-    rw = del_q / (k ** (2) * k)
-    rm = Va / (k ** (2) * k)
-    rh = del_T / (k ** (2) * k)
-    return (rw, rm, rh)
 
 
-# eqs5.21
-# def phi_integrated(State,Grid,App,i,y,x):
-#     k = constants.physical_constants().constants("von karman constant")
-#     Ksi = MO_length(State,Grid,App,i)[2]
-#     if Ksi < -1.574:
-#         phi_itg = (y  + 1.35 * k ** (2/3) * (y)**(2/3) -
-#                     (x  + 1.35 * k ** (2/3) * (x)**(2/3)))
-#     elif Ksi >= -1.574 and Ksi < 0:
-#         phi_itg = (y + ((1 - 16 * y)**(0.5))/8 -
-#                     x + ((1 - 16 * x)**(0.5))/8)
-#     elif Ksi >= 0 and Ksi <1:
-#         phi_itg = (y + 2.5 * y ** (2) -
-#                     (x + 2.5 * x ** (2)))
-#     else:
-#         phi_itg = (5 * y + 0.5 * y ** (2) -
-#                     (5 * x + 0.5 * x ** (2) ))
-#     return phi_itg